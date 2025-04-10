23ac5892da19cc19df170d53dc158fc3c6fc79e2
andrewatwood <sandurz@gmail.com>
2025-04-10 15:08:19 -0500

    Cleanup test case

--- packages/core/test/coreConfiguration.ts
+++ packages/core/test/coreConfiguration.ts
@@ -16,7 +16,6 @@ export const EXHAUSTIVE_INIT_CONFIGURATION: Required<InitConfiguration> = {
   telemetrySampleRate: 60,
   silentMultipleInit: true,
   sessionPersistence: 'cookie',
-  allowFallbackToInMemoryStorage: true,
   allowFallbackToLocalStorage: true,
   allowUntrustedEvents: true,
   storeContextsAcrossPages: true,
@@ -77,7 +76,6 @@ export type MapInitConfigurationKey<Key extends string> =
           | 'internalAnalyticsSubdomain'
           | 'replica'
           | 'enableExperimentalFeatures'
-          | 'allowFallbackToInMemoryStorage'
       ? never
       : // Other keys are simply snake cased
         CamelToSnakeCase<Key>
